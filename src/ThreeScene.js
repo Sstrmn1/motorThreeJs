3898dd090bef4b54166b0454a104d1499bbef90c
Fabio <sstrmn1@gmail.com>
2023-08-24 14:11:56 -0400

    generar caja de prueba

--- src/ThreeScene.js
+++ src/ThreeScene.js
@@ -23,27 +23,45 @@ function ThreeScene() {
     sceneRef.current.appendChild(renderer.domElement);
 
     // Crear pallet
-    const geometry = new THREE.BoxGeometry(1.2, 0.155, 1.2);
+    const palletGeometry = new THREE.BoxGeometry(1.2, 0.155, 1.2);
+    const palletTextureLoader = new THREE.TextureLoader();
+    const palletTexture1 = palletTextureLoader.load("/textures/3.png");
+    const palletTexture2 = palletTextureLoader.load("/textures/2.png");
+    const palletTexture3 = palletTextureLoader.load("/textures/1.png");
 
-    // Cargar las texturas
-    const textureLoader = new THREE.TextureLoader();
-    const texture1 = textureLoader.load("/textures/3.png");
-    const texture2 = textureLoader.load("/textures/2.png");
-    const texture3 = textureLoader.load("/textures/1.png");
-
-    const materials = [
-      new THREE.MeshBasicMaterial({ map: texture2 }), // Lado izquierdo/derecho
-      new THREE.MeshBasicMaterial({ map: texture2 }), // Lado izquierdo/derecho
-      new THREE.MeshBasicMaterial({ map: texture3 }), // Frente/trasero
-      new THREE.MeshBasicMaterial({ map: texture3 }), // Frente/trasero
-      new THREE.MeshBasicMaterial({ map: texture1 }), // Tapa
-      new THREE.MeshBasicMaterial({ map: texture1 })  // Tapa
+    const palletMaterials = [
+      new THREE.MeshBasicMaterial({ map: palletTexture2 }), // Lado izquierdo/derecho
+      new THREE.MeshBasicMaterial({ map: palletTexture2 }), // Lado izquierdo/derecho
+      new THREE.MeshBasicMaterial({ map: palletTexture3 }), // Frente/trasero
+      new THREE.MeshBasicMaterial({ map: palletTexture3 }), // Frente/trasero
+      new THREE.MeshBasicMaterial({ map: palletTexture1 }), // Tapa
+      new THREE.MeshBasicMaterial({ map: palletTexture1 })  // Tapa
     ];
 
-    const cube = new THREE.Mesh(geometry, materials);
-    scene.add(cube);
+    const pallet = new THREE.Mesh(palletGeometry, palletMaterials);
+    scene.add(pallet);
 
-    // Add a grid as the floor
+    // Crear caja
+    const boxGeometry = new THREE.BoxGeometry(0.3, 0.4, 0.2);
+    
+    // Cargar la textura del cartón
+    const textureLoader = new THREE.TextureLoader();
+    const cartonTexture = textureLoader.load("/textures/carton.jpg");
+    
+    const boxMaterials = [
+      new THREE.MeshBasicMaterial({ map: cartonTexture }), // Lado izquierdo
+      new THREE.MeshBasicMaterial({ map: cartonTexture }), // Lado derecho
+      new THREE.MeshBasicMaterial({ map: cartonTexture }), // Frente
+      new THREE.MeshBasicMaterial({ map: cartonTexture }), // Trasero
+      new THREE.MeshBasicMaterial({ map: cartonTexture }), // Arriba
+      new THREE.MeshBasicMaterial({ map: cartonTexture })  // Abajo
+    ];
+    
+    const box = new THREE.Mesh(boxGeometry, boxMaterials);
+    box.position.set(0, 0.155 + 0.2 / 2, 0); // Ubicar encima del pallet
+    scene.add(box);
+
+    // Agregar una rejilla como el piso
     const grid = new THREE.GridHelper(10, 10, 0xffffff, 0x000000);
     grid.position.y = -0.1;
     scene.add(grid);
@@ -59,9 +77,6 @@ function ThreeScene() {
     // Animation loop
     function animate() {
       requestAnimationFrame(animate);
-
-      // No rotation applied to the cube
-
       renderer.render(scene, camera);
     }
 
